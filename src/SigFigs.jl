13edfdbdb7362a0e231e14e915bedaab7d90ab11
casey kneale <ckneale@gmail.com>
2020-04-05 10:34:38 -0400

    Init commit

--- src/SigFigs.jl
+++ src/SigFigs.jl
@@ -1,5 +1,37 @@
 module SigFigs
+    using Base: +, -, /, * , %, transpose, inv
+    mutable struct SigFig
+        value::Real
+        figures::Int
+    end
 
-greet() = print("Hello World!")
+    Base.:(==)( a::SigFig, b::SigFig )::Bool = ( a.value == b.value ) && ( a.figures == b.figures )
 
+    signficantfigures(a::SigFig) = round(a.value, sigdigits = a.figures)
+
+    SigFig( values::AbstractArray{Real}, figures::UInt )::AbstractArray{SigFig} = [ SigFig(v, figures) for v in values ]
+    vals( sfs::AbstractArray{SigFig} ) = map( sf -> sf.value, sfs )
+    figs( sfs::AbstractArray{SigFig} ) = map( sf -> sf.figure, sfs )
+
+    for s in (:+, :-, :/, :*, :^, :%)
+        @eval begin
+            Base.$s(sf::SigFig, value::Real)::SigFig = SigFig( Base.$s(sf.value, value), sf.figures )
+            Base.$s(sf::SigFig, sf2::SigFig)::SigFig = SigFig( Base.$s(sf.value, sf2.value), min( sf.figures, sf2.figures ) )
+            Base.$s(value::Real, sf::SigFig)::SigFig = SigFig( Base.$s(value,sf.value), sf.figures )
+        end
+    end
+
+    #1-1 fns are just 1-1 fns
+    for s in (:cos, :sin, :tan, :tanh, :cosh, :sinh, :atan, :acos, :asin, :log, :exp, :log10, :sqrt)
+        @eval Base.$s(sf::SigFig)::SigFig = SigFig( Base.$s(sf.value), sf.figures )
+    end
+
+    #not optimized but OKAY
+    Base.transpose(a::AbstractArray{SigFig})::AbstractArray{SigFig} = permutedims( a )
+    Base.transpose(a::SigFig) = a
+
+    #Think this one is true? Should handcheck it...
+    #Base.inv(a::AbstractArray{SigFig})::AbstractArray{SigFig} =  ( Base.inv( vals( a ) ), reduce(min, figs( a )) )
+
+    export SigFig, vals, figs, signficantfigures
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,17 @@ using Test
 
 @testset "SigFigs.jl" begin
     # Write your own tests here.
+    @test signficantfigures( SigFig(35, 1) ) == 40
+    @test signficantfigures( SigFig(330, 1) ) == 300
+    @test signficantfigures( SigFig(333, 2) ) == 330
+    @test signficantfigures( sqrt(SigFig(144, 1)) ) == 10
+    @test signficantfigures( SigFig(10.1, 3) + SigFig(10.5, 2) ) == 21
+    @test signficantfigures( SigFig(10.1, 3) + SigFig(10.1, 2) ) == 20
+    @test signficantfigures( SigFig(10.1, 3) + 10 ) == 20.1
+    @test signficantfigures( 11.0 + SigFig(10.1, 3) ) == 21.1
+    #Test basic linear algebra stuff
+    actual = [3,3] * Base.transpose([3,3] )
+    @test all(vals([SigFig(3,2), SigFig(3,1)] * Base.transpose([ SigFig(3,2), SigFig(3,1) ] )) .== actual)
+    @test transpose([ SigFig(3,2), SigFig(3,1) ]) == [ SigFig(3,2) SigFig(3,1)]
+    @test (Base.transpose([SigFig(3,2), SigFig(3,1)]) * [ SigFig(3,2), SigFig(3,1) ]) == SigFig(18,1)
 end
